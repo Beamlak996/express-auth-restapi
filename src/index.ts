e723b745e2b7b89d60018343c8b14844ac21c8ae
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-04 11:47:47 -0900

    Mongo Connection Complete

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express()
 
@@ -18,6 +19,13 @@ app.use(bodyParser.json());
 const server = http.createServer(app)
 
 
-server.listen(8080, ()=> {
+server.listen(process.env.PORT, ()=> {
     console.log("Server runing on port 8080")
 })
+
+const MONGO_URL =
+  "mongodb+srv://beamlak:beamlak@cluster0.spnf0gh.mongodb.net/test";
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error)=> console.log(error))
