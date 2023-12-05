06e7532659d57c602ba92ef271bd14b7d4e37cf5
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-04 21:33:04 -0900

    finished

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,33 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+
+import router from "./router";
 import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    credentials: true
-}))
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-
-server.listen(process.env.PORT, ()=> {
-    console.log("Server runing on port 8080")
-})
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080/");
+});
 
 const MONGO_URL =
   "mongodb+srv://beamlak:beamlak@cluster0.spnf0gh.mongodb.net/test";
 
-mongoose.Promise = Promise
+mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
-mongoose.connection.on('error', (error: Error)=> console.log(error))
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
